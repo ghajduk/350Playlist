3b3a5f91f5b92302f1a0e80cadcf74b05353d058
Kinzie Brooks <kfox@mail.umw.edu>
2013-02-25 21:47:01 -0500

    Blog Template Load
    Loading all the files for the website.

---
+++ db_connect.php
@@ -0,0 +1,5 @@
+<?php
+
+  	$db = mysqli_connect('localhost', 'play', 'list', 'playlist')
+		or die (mysqli_error());
+?>
---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+
+<head>
+  <title>350Playlist</title>
+  <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+  <!-- **** layout stylesheet **** -->
+  <link rel="stylesheet" type="text/css" href="style/style.css" />
+  <!-- **** colour stylesheet **** -->
+  <link rel="stylesheet" type="text/css" href="style/colour.css" />
+</head>
+
+<body>
+  <div id="main">
+    <div id="logo"><h1>350Playlist</h1></div>
+    <div id="menubar">
+      <ul id="menu">
+        <li><a class="current" href="index.php" title="Sidebar - Left">Home</a></li>
+        <li><a href="login.php" title="Login">Login</a></li>
+        <li><a href="search.php" title="Search">Search</a></li>
+        <li><a href="playlistpage.php" title="Playlist">Playlist</a></li>
+      </ul>
+      
+    </div>
+    <div id="site_content">
+      <div class="sidebar">
+        <div class="sidebaritem">
+          <h3>Most Popular</h3>
+          <ul>
+            <li><a class="current" href="#" title="Home">Home</a></li>
+            <li><a href="#" title="">Category 1 (0)</a></li>
+          </ul>
+        </div>
+       
+        <div class="sidebaritem">
+          <h3>Recent Comments</h3>
+          <ul>
+            <li><a href="#" title="">Comment 1</a></li>
+            <li><a href="#" title="">Comment 2</a></li>
+          </ul>
+        </div>
+      </div>
+      <div id="content">
+        <h1>Welcome to the 350Playlist</h1>
+        <!-- **** INSERT PAGE CONTENT HERE **** -->
+        <p>
+          Sick of YouTube?  Join us!
+        </p>
+        <p>
+          Need to create an account? <a href="createAccount.php" title="createaccount">Click here.</a>
+        </p>
+        
+      </div>
+    </div>
+    <div id="footer">
+      Copyright &copy; Company Name | <a href="http://validator.w3.org/check?uri=referer">XHTML 1.1</a> | <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a> | <a href="http://www.dcarter.co.uk">design by dcarter</a> | <a href="#">Login</a>
+    </div>
+  </div>
+</body>
+</html>
